f54f77fdf4507473a3e6ece181bd0b0bc19d05a7
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-06-22 23:19:28 -0300

    estudo API

--- lotofacil/script.js
+++ lotofacil/script.js
@@ -2,12 +2,15 @@
 //let pesquisar = document.querySelector("#pesquisar");
 
 
-let token = "ZSRljdVcBnn1Meq"
-
-let url = `https://loteriascaixa-api.herokuapp.com/api/lotofacil/${token
-}${pesquisar}`
-    function pesquisar(url){
-    fetch(url).then(function (response) {
+    function pesquisar(concurso){
+    fetch(`https://loteriascaixa-api.herokuapp.com/api/lotofacil/${pesquisar}`).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+        })
+    });
+}
+function pesquisarUltimo(){
+    fetch("https://loteriascaixa-api.herokuapp.com/api/lotofacil/2844").then(function (response) {
         response.json().then(function (data) {
             console.log(data)
         })
