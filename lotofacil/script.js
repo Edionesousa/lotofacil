6dd547896dda76728e96805919c7a77136d4a0c2
Edione sousa <117118716+Edionesousa@users.noreply.github.com>
2023-06-30 13:24:50 -0300

    estudo js

--- lotofacil/script.js
+++ lotofacil/script.js
@@ -1,65 +1,45 @@
 
 
-   
-   const Api =`https://loteriascaixa-api.herokuapp.com/api/lotofacil/2848/`
-   let dados = "";
-   
-   fetch(Api)
-    .then(response => {
-        (response.json()).then(body => {
-            const corpo = console.log(body)
-            document.querySelector("#num0").innerHTML = body.dezenas[0];
-            document.querySelector("#num1").innerHTML = body.dezenas[1];
-            document.querySelector("#num2").innerHTML = body.dezenas[2];
-            document.querySelector("#num3").innerHTML = body.dezenas[3];
-            document.querySelector("#num4").innerHTML = body.dezenas[4];
-            document.querySelector("#num5").innerHTML = body.dezenas[5];
-            document.querySelector("#num6").innerHTML = body.dezenas[6];
-            document.querySelector("#num7").innerHTML = body.dezenas[7];
-            document.querySelector("#num8").innerHTML = body.dezenas[8];
-            document.querySelector("#num9").innerHTML = body.dezenas[9];
-            document.querySelector("#num10").innerHTML = body.dezenas[10];
-            document.querySelector("#num11").innerHTML = body.dezenas[11];
-            document.querySelector("#num12").innerHTML = body.dezenas[12];
-            document.querySelector("#num13").innerHTML = body.dezenas[13];
-            document.querySelector("#num14").innerHTML = body.dezenas[14];
-            
-            //Estado ganhador
-            
-                const estado =  document.querySelector(".section #nome").innerHTML = body.estadosPremiados[0].nome;
-                const uf = document.querySelector(".section #uf" ).innerHTML = body.estadosPremiados[0].uf;
+
+//const Api = `https://loteriascaixa-api.herokuapp.com/api/lotofacil/${pesquisar}/`
+
+
+
+
+
+ function pesquisar(){
+   const valor = document.querySelector("#pesquisar").value;
+
+   console.log()
+
+}
+
+
+
+/*async function popularTela(click){
+    await fetch(Api).then(response => {
+            (response.json()).then(body => {
+                let = document.querySelector("#num0").innerHTML = body.dezenas[0];
+                let = document.querySelector("#num1").innerHTML = body.dezenas[1];
+                let = document.querySelector("#num2").innerHTML = body.dezenas[2];
+                let = document.querySelector("#num3").innerHTML = body.dezenas[3];
+                let = document.querySelector("#num4").innerHTML = body.dezenas[4];
+                let = document.querySelector("#num5").innerHTML = body.dezenas[5];
+                let = document.querySelector("#num6").innerHTML = body.dezenas[6];
+                let = document.querySelector("#num7").innerHTML = body.dezenas[7];
+                let = document.querySelector("#num8").innerHTML = body.dezenas[8];
+                let = document.querySelector("#num9").innerHTML = body.dezenas[9];
+                let = document.querySelector("#num10").innerHTML = body.dezenas[10];
+                let = document.querySelector("#num11").innerHTML = body.dezenas[11];
+                let = document.querySelector("#num12").innerHTML = body.dezenas[12];
+                let = document.querySelector("#num13").innerHTML = body.dezenas[13];
+                let = document.querySelector("#num14").innerHTML = body.dezenas[14];
                 
-            // premiaçoes por ponto
-            const pontos = 
-                document.querySelector(".section #acertos").innerHTML = body.premiacoes[0].acertos;
-                document.querySelector(".section #vencedores").innerHTML = body.premiacoes[0].vencedores + "  vencedor";
-                document.querySelector(".section #premio").innerHTML = body.premiacoes[0].premio + "  premio";
-
-
-
-            
-                const pontos14 = document.querySelector(".section #Pts").innerHTML = body.premiacoes[0];
-                const pontos13 = document.querySelector(".section #Pts").innerHTML = body.premiacoes[0];
-                const pontos12 = document.querySelector(".section #Pts").innerHTML = body.premiacoes[0];
-                const pontos11 = document.querySelector(".section #Pts").innerHTML = body.premiacoes[0];
-            
-        })
-    })
-
-    
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                //Estado ganhador
+                document.querySelector(".section #nome").innerHTML = body.estadosPremiados[0].nome;
+                document.querySelector(".section #uf").innerHTML = body.estadosPremiados[0].uf;
+                document.querySelector(".section footer").innerHTML = body.concurso;
+                body = console.log(body)
+            })
+})
+*/
